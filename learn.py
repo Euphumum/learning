b35063c7280a09d16cf1ee69bedbbe95c4ff1068
Euphumum <jonahhernandez32@gmail.com>
2019-09-30 09:03:13 -0500

    I played with print and octothorpe.

--- learn.py
+++ learn.py
@@ -1,12 +1,17 @@
-print("hello world")
+# this is and octothorpe -- it makes a comment (line ignored ny the computer)
+# print("hello world")
 print("Howdy, y'all")
 print("I like typing this.")
 print("This is fun.")
 
+# math skills demo
 
-
-
-
+print("I will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now I will count the eggs", 7)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 < 5 - 7)
 
 
 
