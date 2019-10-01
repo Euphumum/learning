b7d49ce2b6317618e28f0bd4d43a42501a13247f
Euphumum <jonahhernandez32@gmail.com>
2019-10-01 08:43:24 -0500

    Explaining what every line does.

--- learn.py
+++ learn.py
@@ -5,12 +5,17 @@ print("I like typing this.")
 print("This is fun.")
 
 # math skills demo
-
+# prints the following sentence
 print("I will not count my chickens", 2)
+# used this code to calculate the number of hens
 print("Hens: ", 25+30/6)
+# used this code to calculate the number roosters
 print("Roosters: ", 100 - 25 * 3 % 4)
+# printed the phrase " Now I will count the eggs"
 print("Now I will count the eggs", 7)
+# used this code to calculate the following equation
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# used this to determine whether the statement was true or false
 print(3 + 2 < 5 - 7)
 
 
