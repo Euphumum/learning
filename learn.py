77489384160425107c9c70030eaa62bb2b1fa874
Euphumum <jonahhernandez32@gmail.com>
2019-10-03 08:40:27 -0500

    More practice with some variables

--- learn.py
+++ learn.py
@@ -18,10 +18,25 @@ print("Now I will count the eggs", 7)
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 # used this to determine whether the statement was true or false
 print(3 + 2 < 5 - 7)
+print("Now I will count the number of roosters and eggs")
+print(50.0 - 25.0 + 5.0 - 10.0)
 
+# variables and some of their powers
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = - drivers
+cars_driven = drivers
+carpool_capacity = space_in_a_car * cars_driven
+average_passengers_per_car = passengers / cars_driven
 
-
-
+print("There are", cars, "cars available")
+print("There are only", drivers, "drivers available today")
+print("There will be", cars_not_driven, "empty cars today")
+print("We cab transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today")
+print("We need to approximately", average_passengers_per_car, "in each car")
 
 
 
